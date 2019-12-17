7c54e2eed7811a70c9aaa2c8ef397f44beeef8a3
Luram Archanjo <luram.archanjo@sensedia.com>
2019-12-16 21:52:13 -0300

    Try-catch resource

--- src/main/java/com/example/HttpClientTest.java
+++ src/main/java/com/example/HttpClientTest.java
@@ -1,25 +1,25 @@
-//package com.example;
-//
-//import java.io.IOException;
-//import java.net.URI;
-//import java.net.URISyntaxException;
-//import jdk.incubator.http.HttpClient;
-//import jdk.incubator.http.HttpRequest;
-//import jdk.incubator.http.HttpResponse;
-//import jdk.incubator.http.HttpResponse.BodyHandler;
-//
-//public class HttpClientTest {
-//
-//  public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
-//    HttpRequest httpRequest = HttpRequest.newBuilder()
-//        .uri(new URI("https://postman-echo.com/get"))
-//        .GET()
-//        .build();
-//
-//    HttpResponse<String> httpResponse = HttpClient.newHttpClient()
-//        .send(httpRequest, BodyHandler.asString());
-//
-//    System.out.println(httpResponse);
-//  }
-//
-//}
+package com.example;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
+import jdk.incubator.http.HttpResponse.BodyHandler;
+
+public class HttpClientTest {
+
+  public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+    HttpRequest httpRequest = HttpRequest.newBuilder()
+        .uri(new URI("https://postman-echo.com/get"))
+        .GET()
+        .build();
+
+    HttpResponse<String> httpResponse = HttpClient.newHttpClient()
+        .send(httpRequest, BodyHandler.asString());
+
+    System.out.println(httpResponse);
+  }
+
+}
